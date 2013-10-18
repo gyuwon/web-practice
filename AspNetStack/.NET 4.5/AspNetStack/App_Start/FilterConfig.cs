0f29a02299d018ef78d307b6176eba5d1a6d9859
Gyuwon <gyuwon@live.com>
2013-10-18 10:05:32 +0900

    ASP.NET 4.5

---
+++ AspNetStack/.NET 4.5/AspNetStack/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetStack
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AspNetStack/.NET 4.5/AspNetStack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetStack.MvcApplication" Language="C#" %>
