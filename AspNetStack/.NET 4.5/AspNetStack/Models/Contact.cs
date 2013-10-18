db33fd23020054e1f0e3c27db7adb33f9cccb150
Gyuwon <gyuwon@live.com>
2013-10-18 10:32:16 +0900

    Contact model

---
+++ AspNetStack/.NET 4.5/AspNetStack/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetStack.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+        [Required, StringLength(50)]
+        public string Email { get; set; }
+    }
+}
