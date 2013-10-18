6da7ce8f7d1fdd5a0bb43012bc4897a4d78eb3a7
Gyuwon <gyuwon@live.com>
2013-10-18 13:38:48 +0900

    201310180432245_Initialize

---
+++ AspNetStack/.NET 4.5/AspNetStack/Migrations/201310180432245_Initialize.cs
@@ -0,0 +1,78 @@
+namespace AspNetStack.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initialize : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserSecrets",
+                c => new
+                    {
+                        UserName = c.String(nullable: false, maxLength: 128),
+                        Secret = c.String(),
+                    })
+                .PrimaryKey(t => t.UserName);
+            
+            CreateTable(
+                "dbo.UserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey });
+            
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserRoles",
+                c => new
+                    {
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.RoleId, t.UserId });
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        ContactId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ContactId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Contacts");
+            DropTable("dbo.UserRoles");
+            DropTable("dbo.Roles");
+            DropTable("dbo.UserLogins");
+            DropTable("dbo.UserSecrets");
+            DropTable("dbo.Users");
+        }
+    }
+}
--- AspNetStack/.NET 4.5/AspNetStack/Migrations/Configuration.cs
+++ AspNetStack/.NET 4.5/AspNetStack/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace AspNetStack.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(AspNetStack.Models.WebStackDbContext context)
