ac7a91507876ef4d3a58809bc828ad23cd20bf12
Gyuwon <gyuwon@live.com>
2013-10-20 04:04:57 +0900

    AngularJS Step 01

---
+++ AspNetStack/.NET 4.5/AspNetStack/Controllers/AngularController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetStack.Controllers
+{
+    public class AngularController : Controller
+    {
+        //
+        // GET: /Angular/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AspNetStack/.NET 4.5/AspNetStack/Controllers/ContactsController.cs
+++ AspNetStack/.NET 4.5/AspNetStack/Controllers/ContactsController.cs
@@ -14,5 +14,5 @@ namespace AspNetStack.Controllers
         {
             return View();
         }
-	}
+    }
 }
