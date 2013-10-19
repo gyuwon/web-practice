cfdb09bf95467c6198a8c350df6d5accf4afeb68
Gyuwon <gyuwon@live.com>
2013-10-20 04:13:41 +0900

    Merge

--- AspNetStack/.NET 4.5/AspNetStack/Controllers/AngularController.cs
+++ AspNetStack/.NET 4.5/AspNetStack/Controllers/AngularController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,23 +15,4 @@ namespace AspNetStack.Controllers
             return View();
         }
     }
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace AspNetStack.Controllers
-{
-    public class AngularController : Controller
-    {
-        //
-        // GET: /Angular/
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
->>>>>>> 6a775c913a72c93db392494d079e4252bbf12dce
 }
