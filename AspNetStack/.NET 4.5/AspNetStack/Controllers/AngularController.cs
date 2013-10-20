8453f95911723ebac451001a7f9927b51ee66c76
Gyuwon <gyuwon@live.com>
2013-10-20 12:54:44 +0900

    Angular action routing

--- AspNetStack/.NET 4.5/AspNetStack/Controllers/AngularController.cs
+++ AspNetStack/.NET 4.5/AspNetStack/Controllers/AngularController.cs
@@ -1,16 +1,42 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Reflection;
 using System.Web.Mvc;
 
 namespace AspNetStack.Controllers
 {
     public class AngularController : Controller
     {
-        //
-        // GET: /Angular/
+        private static readonly string LastStep = AngularController.GetLastStepActionName();
+
+        private static string GetLastStepActionName()
+        {
+            Type t = typeof(AngularController);
+            MethodInfo action = (
+                from m in t.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                where
+                    (
+                        m.ReturnType.Equals(typeof(ActionResult)) ||
+                        m.ReturnType.IsSubclassOf(typeof(ActionResult))
+                    ) &&
+                    m.Name.StartsWith("Step")
+                orderby m.Name descending
+                select m
+            ).FirstOrDefault();
+            return action == null ? string.Empty : action.Name;
+        }
+
         public ActionResult Index()
+        {
+            return this.RedirectToAction(AngularController.LastStep);
+        }
+
+        public ActionResult Step00()
+        {
+            return View();
+        }
+
+        public ActionResult Step01()
         {
             return View();
         }
