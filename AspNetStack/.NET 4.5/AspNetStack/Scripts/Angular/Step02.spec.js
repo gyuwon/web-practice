65ecc1044ad75aaea3103abad3e8b174b6f9b142
Gyuwon <gyuwon@live.com>
2013-10-24 23:19:46 +0900

    AngularJS Step 02

--- AspNetStack/.NET 4.5/AspNetStack/Scripts/Angular/Step02.spec.js
+++ AspNetStack/.NET 4.5/AspNetStack/Scripts/Angular/Step02.spec.js
@@ -1,10 +1,12 @@
-﻿describe('PhoneCat controllers', function () {
-    describe('PhoneListCtrl', function () {
-        beforeEach(module('phonecatApp'));
-        it('should create "phones" model with 3 phones', inject(function ($controller) {
-            var scope = {},
-                ctrl = $controller('PhoneListCtrl', { $scope: scope });
-            expect(scope.phones.length).toBe(3);
-        }));
+﻿describe('AngularJS Step 02', function () {
+    describe('PhoneCat controllers', function () {
+        describe('PhoneListCtrl', function () {
+            beforeEach(module('phonecatApp'));
+            it('should create "phones" model with 3 phones', inject(function ($controller) {
+                var scope = {},
+                    ctrl = $controller('PhoneListCtrl', { $scope: scope });
+                expect(scope.phones.length).toBe(3);
+            }));
+        });
     });
 });
