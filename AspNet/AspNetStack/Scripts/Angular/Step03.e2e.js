8ae40707c11027b1f22b550a708f7c5296b1756c
Gyuwon <gyuwon@live.com>
2013-10-28 23:41:06 +0900

    Restructure

---
+++ AspNet/AspNetStack/Scripts/Angular/Step03.e2e.js
@@ -0,0 +1,26 @@
+﻿describe('AngularJS Step 03', function () {
+    describe('PhoneCap App', function () {
+        describe('Phone List View', function () {
+            beforeEach(function () {
+                browser().navigateTo('../../Angular/Step03');
+            });
+            it('should filter the phone list as user types into the search box', function () {
+                expect(repeater('.phones li').count()).toBe(3);
+
+                input('query').enter('nexus');
+                expect(repeater('.phones li').count()).toBe(1);
+
+                input('query').enter('motorola');
+                expect(repeater('.phones li').count()).toBe(2);
+            });
+            it('should display the current filter value within an element with id "status"', function () {
+                expect(element('#status').text()).toMatch(/Current filter: \s*$/);
+
+                input('query').enter('nexus');
+                expect(element('#status').text()).toMatch(/Current filter: nexus\s*$/);
+
+                pause();
+            });
+        });
+    });
+});
